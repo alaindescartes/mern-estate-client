78e6ebacd4b79aefd7104c76c34a3d18f1313e88
Alain Descartes <ualaindescartes@gmail.com>
2024-09-10 11:55:08 -0600

    added initial header ui

--- src/pages/RootWrapper.jsx
+++ src/pages/RootWrapper.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 function RootWrapper() {
   return (
     <>
-      <div>hey</div>
+      <Header />
       <Outlet />
     </>
   );
