43bc49f02c341a978d33021fb7d96f1e8f313f81
alain descartes <ualaindescartes@gmail.com>
2024-09-14 18:37:38 -0600

    create a middleware and function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -18,10 +18,21 @@ mongoose
     console.log("Error: " + err);
   });
 
-//setting up routes
+//middleware
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoute);
 
 app.listen(3000, () => {
   console.log("server is running on port 3000!!");
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
